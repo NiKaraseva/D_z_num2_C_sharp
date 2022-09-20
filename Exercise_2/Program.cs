3ef20688510c3ba0673d6b088e1679cb81064f2d
Nika <nikarasevas@gmail.com>
2022-09-20 23:15:51 +0300

    Вывели 3-ю цифру заданного числа или сообщили, что её нет

---
+++ Exercise_2/Program.cs
@@ -0,0 +1,19 @@
+﻿/* Программа, которая выводит третью цифру заданного числа или сообщает, 
+что третьей цифры нет.*/
+
+Console.WriteLine("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+
+if (number >= 100)
+{
+    while (number > 999)
+    {
+        number = number / 10;
+    }
+    Console.WriteLine($"Третья цифра заданного числа {number % 10}");
+}
+
+if (number < 100)
+{
+  Console.WriteLine("Третьей цифры нет");  
+}
